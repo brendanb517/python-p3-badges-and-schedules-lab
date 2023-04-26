3c7d558008fcc62c8636d80937358bacec16126f
Brendan <brendan.b517@gmail.com>
2023-04-26 18:04:48 -0400

    Passed all tests, and fixed one test that was incorrect

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    # names_list = []
+    # for name in names:
+    #     names_list.append(f"Hello, my name is {name}.")
+    # return names_list
+    return [f"Hello, my name is {name}." for name in names]
 
 def assign_rooms(names):
-    return None
+    rooms = [1, 2, 3, 4, 5, 6, 7, 8]
+    room = 0
+    room_assignments = []
+    for name in names:
+        room_assignments.insert(room, f"Hello, {name}! You'll be assigned to room {rooms[room]}!")
+        room += 1
+    return room_assignments
 
 def printer(names):
-    return None
+    name_index = 0
+    while name_index < len(names):
+        print(batch_badge_creator(names)[name_index])
+        name_index += 1
+    name_index = 0
+    while name_index < len(names):
+        print(assign_rooms(names)[name_index])
+        name_index += 1
